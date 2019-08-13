59c5a10a54d13d99ee60fbb823c6599a470dd243
ar <arnxus@gmail.com>
2019-08-12 19:21:51 -0600

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pycore',
     version='0.1.0',
-    packages=['',],
-    package_dir={'pycore': 'src/pycore',
-    'tests': 'tests'},
-    long_description=open('README.txt').read(),
+    packages=find_packages(),
+    package_dir={'pycore': 'src/pycore'},
 )
