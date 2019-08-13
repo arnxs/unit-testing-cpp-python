11422132834e3e9db211a060308455718c4b555b
ar <arnxus@gmail.com>
2019-08-12 18:17:14 -0600

    run pytest-watch with testmon flag

--- go.sh
+++ go.sh
@@ -3,5 +3,8 @@
 # set PYTHONPATH to ./src
 source .env
 
-# pytest watch
-ptw
+# pytest watch (ptw) - watch changes to trigger test-runner
+# ptw
+
+# Only run tests that are affected
+ptw -- --testmon
