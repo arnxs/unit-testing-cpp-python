1f52b86133dc7e5b8c03007f6022422225f0d828
ar <arnxus@gmail.com>
2019-08-14 21:29:24 -0600

    add catch2/examples

--- Makefile
+++ Makefile
@@ -3,16 +3,21 @@
 
 LIB_DIR=lib
 TEST_DIR=tests
+EXTERNAL_DIR=external
 SRC_DIR=src
 CPP_FLAGS=-std=c++11 -Wall
 
 INCLUDES=\
 	-I$(TEST_DIR)\
+	-I$(EXTERNAL_DIR)\
 	-I$(SRC_DIR)/core/common
 
 SRCS=\
-	$(TEST_DIR)/core/common/test_utils.cpp\
-	$(SRC_DIR)/core/common/utils.cpp
+	$(TEST_DIR)/core/common/test_utils.cpp \
+	$(SRC_DIR)/core/common/utils.cpp \
+	$(TEST_DIR)/catch2/examples/000-CatchMain.cpp \
+	$(TEST_DIR)/catch2/examples/010-TestCase.cpp \
+	$(TEST_DIR)/catch2/examples/020-TestCase-1.cpp
 
 OBJS=$(SRCS:.cpp=.o)
 
--- tests/core/common/test_utils.cpp
+++ tests/core/common/test_utils.cpp
@@ -1,5 +1,8 @@
+#ifndef CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
+#endif
+
+#include <catch2/catch.hpp>
 #include "utils.hpp"
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
@@ -7,4 +10,25 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
     REQUIRE( Factorial(10) == 3628800 );
+}
+
+TEST_CASE( "vectors can be sized and resized", "[vector]" ) {
+    // initialization block executed for each section
+    std::vector<int> v( 5 );
+    REQUIRE( v.size() == 5 );
+    REQUIRE( v.capacity() >= 5 );
+    // end of initialization block
+
+    SECTION( "resizing bigger changes size and capacity" ) {
+         v.resize( 10 );
+
+         REQUIRE( v.size() == 10 );
+         REQUIRE( v.capacity() >= 10 );
+    }
+    SECTION( "resizing smaller changes size but not capacity" ) {
+        v.resize( 0 );
+
+        REQUIRE( v.size() == 0 );
+        REQUIRE( v.capacity() >= 5 );
+    }
 }
