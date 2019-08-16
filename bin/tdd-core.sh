89577863d16dcbd9ce24fa834d902dd052954cc4
ar <arnxus@gmail.com>
2019-08-15 22:14:17 -0600

    modify C++ TDD scripts to work in linux environment

--- bin/tdd-core.sh
+++ bin/tdd-core.sh
@@ -8,5 +8,13 @@ cd core
 make test
 
 # use fswatch to monitor changes in C++ files and trigger tests
-fswatch -o -i "*.*pp" --event=1 ./src/core ./tests | xargs -n 1 -I {} make test
 
+#for MacOS
+# fswatch -o -i "*.*pp" --event=1 ./src/core ./tests | xargs -n 1 -I {} make test
+
+#for Linux
+FORMAT=$(echo -e "\033[1;33m%w%f\033[0m written")
+while inotifywait -qre close_write --format "$FORMAT" .
+do
+    make test
+done
